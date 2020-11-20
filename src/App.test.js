3cf632789524842334bf1c5706c3a1138363b9ef
joardar-aditya <youngjoardar@gmail.com>
2020-11-20 10:39:42 +0530

    app.test deleted

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { fireEvent, getByPlaceholderText, render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import App from './App';
-
-it('renders Search lable', () => {
-  render(<App />);
-  expect(screen.getByText('Search for Guest List')).toBeInTheDocument();
-});
